564708456ce388e9d786c19c08f845c9af8342fe
bizutilaye <bizualemtilaye@gmail.com>
2023-03-02 00:42:18 -0800

    Update

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+cards = []
+suits = ["speads", "clubs", "hearts", "diamonds"]
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+for suit in suits:
+    for rank in ranks:
+        cards.append([suit,rank])
+
+random.shuffle(cards)
+card = cards.pop()
+print(card)
